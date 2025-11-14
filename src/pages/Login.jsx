ac0d882d5beb8c0584af3b4397babbe5159b5aae
Dhanush A S <dhanushbharath2056@gmail.com>
2025-11-14 15:49:46 +0530

    Added React env

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
     setError("");
 
     try {
-      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/login`, formData);
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, formData);
       localStorage.setItem("token", res.data.token);
       alert("Login successful!");
       navigate("/dashboard"); // redirect to Home page
